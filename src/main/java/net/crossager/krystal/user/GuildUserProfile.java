340448f6d6b8db8966e43345fbb9ebb2b6f4f52f
OohBen <bengoihman@gmail.com>
2022-09-23 19:12:05 -0400

    Changes by corss

---
+++ src/main/java/net/crossager/krystal/user/GuildUserProfile.java
@@ -0,0 +1,34 @@
+package net.crossager.krystal.user;
+
+import net.crossager.krystal.guild.GuildProfile;
+
+public class GuildUserProfile {
+    private final long id;
+    private final GuildProfile guildProfile;
+
+    private long money;
+
+    public GuildUserProfile(long id, GuildProfile guildProfile) {
+        this.id = id;
+        this.guildProfile = guildProfile;
+    }
+
+    public GuildUserProfile setMoney(long money) {
+        this.money = money;
+        return this;
+    }
+
+    public GuildUserProfile addMoney(long money) {
+        this.money += money;
+        return this;
+    }
+
+    public GuildUserProfile removeMoney(long money){
+        this.money -= money;
+        return this;
+    }
+
+    public long getMoney() {
+        return money;
+    }
+}
